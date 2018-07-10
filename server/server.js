b85647093b3717b2a0f7bbb03c4885c2f5af2613
Niek <niek_vs@hotmail.com>
2018-07-10 11:46:43 +0200

    Add PATCH /todos/id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+const _ = require('lodash');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -41,7 +42,7 @@ app.get('/todos/:id', (req, res) => {
 
     Todo.findById(id).then((todo) => {
         if (todo) {
-            return res.status(200).send({todo});
+            return res.status(200).send({ todo });
         }
         else {
             // console.log('Valid but not found');
@@ -53,22 +54,50 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
-app.delete('/todos/:id', (req,res)=>{
+app.delete('/todos/:id', (req, res) => {
     var id = req.params.id;
-    if(!ObjectID.isValid(id)){
+    if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
 
-    Todo.findByIdAndRemove(id).then((todo)=>{
-        if(todo){
-            return res.status(200).send({todo});
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (todo) {
+            return res.status(200).send({ todo });
         }
         else {
             return res.status(404).send();
         }
-    }).catch((error)=>{
+    }).catch((error) => {
         res.status(400).send();
     });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    }
+    else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({ todo });
+    }).catch((error) => {
+        res.status(400).send();
+    })
+
+
 });
 
 app.listen(port, () => {
