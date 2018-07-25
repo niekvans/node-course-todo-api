2cafcb819f36524e05d02f14d2dc0e680a43776e
Niek <niek_vs@hotmail.com>
2018-07-25 15:01:49 +0200

    Add authentication

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ const _ = require('lodash');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -110,19 +111,27 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 app.post('/users', (req, res) => {
-    var body = _.pick(req.body,['email', 'password']);
+    var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
 
     user.save().then(() => {
         return user.generateAuthToken();
-    }).then((token)=>{
-        res.header('x-auth',token).send(user);
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
     })
-    .catch((error) => {
-        console.log(error);
-        res.status(400).send(error);
-    })
-})
+        .catch((error) => {
+            console.log(error);
+            res.status(400).send(error);
+        })
+});
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+
+    res.send(req.user);
+
+});
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
